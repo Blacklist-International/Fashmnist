eb4da0e79dd357eae3daad35bd05b4d5c847d668
Kelvin <123539057+Primezane@users.noreply.github.com>
2023-01-27 12:01:51 +0700

    add : model.py

---
+++ model.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+train_images = tf.cast(train_images, tf.float32) / 255.0
+test_images = tf.cast(test_images, tf.float32) / 255.0
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(units=256, activation=tf.nn.relu),
+    tf.keras.layers.Dense(units=128, activation=tf.nn.relu),
+    tf.keras.layers.Dense(units=10, activation=tf.nn.softmax),
+])
+
+model.compile(
+    optimizer='adam',
+    loss='SparseCategoricalCrossentropy',
+    metrics=['accuracy']
+)
+
+history = model.fit(
+    train_images, train_labels, 
+    validation_data=(test_images, test_labels), 
+    epochs=5, verbose=2
+)
+     
